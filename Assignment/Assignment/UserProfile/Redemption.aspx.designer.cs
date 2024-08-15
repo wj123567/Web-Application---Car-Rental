2fd0fcc1893355ad6730d8474e504ccfd18a1c8a
You1234why <limxy-pm23@student.tarc.edu.my>
2024-08-15 11:03:26 +0800

    asd

--- Assignment/Assignment/UserProfile/Redemption.aspx.designer.cs
+++ Assignment/Assignment/UserProfile/Redemption.aspx.designer.cs
@@ -15,12 +15,12 @@ namespace Assignment
     {
 
         /// <summary>
-        /// lvRedemptionItems control.
+        /// SqlDataSource1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView lvRedemptionItems;
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
     }
 }
