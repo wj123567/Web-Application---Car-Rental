92b446b4b26ef6b0c69ab8cfeac27a40cea1365e
You1234why <limxy-pm23@student.tarc.edu.my>
2024-08-15 15:20:51 +0800

    asd

--- Assignment/Assignment/UserProfile/Redemption.aspx.designer.cs
+++ Assignment/Assignment/UserProfile/Redemption.aspx.designer.cs
@@ -22,5 +22,14 @@ namespace Assignment
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+
+        /// <summary>
+        /// rptRedemption control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Repeater rptRedemption;
     }
 }
