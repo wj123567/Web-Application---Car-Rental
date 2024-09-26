c89fa4712a4c43dc82d63968d18b60f4ecd45bea
You1234why <limxy-pm23@student.tarc.edu.my>
2024-09-26 11:16:05 +0800

    db

---
+++ Assignment/Assignment/Models/PointsRecord.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment.Models
+{
+    [Serializable]
+    public class PointsRecord
+    {
+        public DateTime Date { get; set; }
+        public int Points { get; set; }
+        public bool IsEarned { get; set; } // true for earned, false for used
+        public string RedeemDescription { get; set; }
+    }
+}
